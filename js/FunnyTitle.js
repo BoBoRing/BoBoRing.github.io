81d4a2cdd500eb71938651deec7e31f552dab9cc
boboring <807362320@qq.com>
2021-11-22 20:48:03 +0800

    Site updated: 2021-11-22 20:47:57

---
+++ js/FunnyTitle.js
@@ -0,0 +1,17 @@
+// 浏览器搞笑标题
+var OriginTitle = document.title;
+var titleTime;
+document.addEventListener('visibilitychange', function () {
+    if (document.hidden) {
+        $('[rel="icon"]').attr('href', "/funny.ico");
+        document.title = '╭(°A°`)╮你要离开阿南了吗 ~';
+        clearTimeout(titleTime);
+    }
+    else {
+        $('[rel="icon"]').attr('href', "/favicon.ico");
+        document.title = '(ฅ>ω<*ฅ) 噫又好啦 ~' + OriginTitle;
+        titleTime = setTimeout(function () {
+            document.title = OriginTitle;
+        }, 2000);
+    }
+});
